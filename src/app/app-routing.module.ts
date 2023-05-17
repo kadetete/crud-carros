1ce00dfd4d7e285dbdc57b096aaf66942609e5e3
kadetete <kadetesan@gmail.com>
2023-05-17 16:20:26 -0300

    configuração das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaComponent } from './lista/lista.component';
+import { FormsComponent } from './forms/forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path:'lista', component: ListaComponent },
+    { path:'forms', component: FormsComponent },
+    { path:'', component: ListaComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListaComponent } from './lista/lista.component';
@@ -12,7 +14,9 @@ import { FormsComponent } from './forms/forms.component';
     FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
