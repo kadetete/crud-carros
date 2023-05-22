4d40743898f96a7f15eba6796b6b2fa852432f7a
kadetete <kadetesan@gmail.com>
2023-05-22 15:36:33 -0300

    preparação para conexão com a API

--- src/app/dados.service.ts
+++ src/app/dados.service.ts
@@ -14,6 +14,18 @@ export class DadosService {
   }
 
   getDados(): Dados[] {
+    console.log(this.dados);
     return this.dados;
+  }
+
+  editarDados(dado: Dados): void {
+    const index = this.dados.findIndex(item => item === dado)
+    if (index !== -1) {
+      this.dados[index] = dado;
+    }
+  }
+
+  deletarDados(dado: Dados): void {
+    this.dados = this.dados.filter(item => item !== dado);
   }
 }
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Dados } from '../dados';
 import { DadosService } from '../dados.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista',
@@ -10,13 +10,30 @@ import { DadosService } from '../dados.service';
 })
 export class ListaComponent implements OnInit{
   dadosFormulario: Dados[] = [];
+  carregando = true;
 
   constructor(private dadosService: DadosService, private router: Router) {
 
   }
+  
 
   ngOnInit(): void {
-    this.dadosFormulario = this.dadosService.getDados();
+    this.onListar();
   }
 
+  onListar(): void {
+    this.dadosFormulario = this.dadosService.getDados()
+  }
+
+  editarDados(dado: Dados): void {
+    this.dadosService.editarDados(dado);
+  }
+
+  deletarDados(dado: Dados): void {
+    this.dadosService.deletarDados(dado);
+  }
+
+  cadastrar(): void {
+    this.router.navigate(['/forms']);
+  }
 }
